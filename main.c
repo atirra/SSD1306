8b334cac469349fbf93df259b53afe2adc4b1248
Nicolas Pannwitz <PIC-Nico@users.noreply.github.com>
2017-08-25 16:30:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,89 +1,69 @@
 /*******************************************************************************
  * File:        main.c
- * Project:     SSD1306 
- * Author:      Nicolas Meyertöns
- * Version:     
+ * Project:     SP18 - I2C OLED Display
+ * Author:      Nicolas MeyertÃ¶ns
+ * Version:     siehe main.h
  * Web:         http://pic-projekte.de
  ******************************************************************************/
 
 #include <xc.h>
-#include <stdbool.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include "main.h"
 #include "lcd.h"
+#include "peri.h"
 
 #pragma config FOSC = INTIO67       // Internal oscillator block
 #pragma config PWRTEN = ON          // Power up timer enabled
 #pragma config BOREN = OFF          // Brown-out Reset disabled
 #pragma config WDTEN = OFF          // Watch dog timer is always disabled
-#pragma config MCLRE = INTMCLR      // MCLR disabled
+#pragma config MCLRE = EXTMCLR      // MCLR enabled
 #pragma config LVP = OFF            // Single-Supply ICSP disabled
 
-/*******************************************************************************
+/*********************************************************************
  * Diverse Einstellungen zum PIC (IO, Takt, ...)
  */
 
 void initPIC(void)
 {
-    LATBbits.LB0 = 1;
-    
     TRISA = 0x00;
-    TRISB = 0x01;                   // RB0 Eingang
-    TRISC = 0x10;                   // C4 Eingang
-    TRISD = 0x03;                   // D0,D1 Eingänge
-    TRISE = 0x00;
+    TRISB = 0x01;                   // RB0:Input (PB)
+    TRISC = 0x38;                   // RC3,RC4,RC6:Input (AN16)
 
-    ANSELA = 0x00;
-    ANSELB = 0x00;
-    ANSELC = 0x00;
-    ANSELD = 0x00;
-    ANSELE = 0x00;
+    ANSELA = 0x00;                  // All digital
+    ANSELB = 0x00;                  // ...
+    ANSELC = 0x20;                  // AN16 on
 
     OSCCON2bits.MFIOSEL = 0;        // 111: 16 MHz
-    OSCCONbits.IRCF2 = 1;           // 110:  8 MHz <-- aktiv
-    OSCCONbits.IRCF1 = 1;           // 101:  4 MHz
-    OSCCONbits.IRCF0 = 0;           // 100:  2 MHz
+    OSCCONbits.IRCF2 = 1;           // 110:  8 MHz
+    OSCCONbits.IRCF1 = 0;           // 101:  4 MHz
+    OSCCONbits.IRCF0 = 0;           // 100:  2 MHz <-- aktiv
 }
 
-/*******************************************************************************
- * Durchführen sämtlicher Initialisierungen beim Bootvorgang
- */
-
-void initAll (void)
-{
-    initPIC();
-    initI2C();
-}
-
-
-void delayMS(uint16_t ms)
-{
-    uint16_t k=0;
-    
-    for(k=0; k<ms; k++)
-    {
-        __delay_ms(1);
-    }
-}
-
-/*******************************************************************************
+/*********************************************************************
  * Main Routine
  */
 
 void main (void)
-{
-    uint16_t k = 0;
-    
-    /*Alle Initialisierungen durchführen*/
-    initAll();
-
+{    
+    initPIC();
+    initI2C();
     lcd_init();
-    
-    fb_draw_string(0,0,"SSD1306");
-    fb_draw_string(0,1,"pic-projekte.de");
+
+    /*Shut OFF the LCD-Backlight*/
+    LATCbits.LC2 = 1;
+
+    /*Etwas Text in den Framebuffer schreiben*/
+    fb_draw_string_big(44,0,"OLED");
+    fb_draw_string_big(0,2,"Typ:  SSD1306");
+    fb_draw_string_big(0,4,"Size: 128x84");
+    fb_draw_string(15,7,"http://pic-projekte.de/");
+
+    /*Den Framebuffer zur Anzeige bringen*/
     fb_show();
     
     /*Endlosschleife*/
     while(1)
-    {   
+    {
     }
 }
